84200d7eb0e5ae817d76a89033cdae0c041c5d73
Jacob Janak <jakeathotmail@gmail.com>
2018-07-30 15:56:04 -0700

    Added in a theme based on the InnovationsCity logo.

--- client/src/theme/theme.js
+++ client/src/theme/theme.js
@@ -3,15 +3,15 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      light: '#757ce8',
-      main: '#3f50b5',
-      dark: '#002884',
+      light: '#707dff',
+      main: '#2151f1',
+      dark: '#0029bd',
       contrastText: '#fff',
     },
     secondary: {
-      light: '#ffffb1',
-      main: '#ffd180',
-      dark: '#caa052',
+      light: '#ffffff',
+      main: '#d1d3d4',
+      dark: '#a0a2a3',
       contrastText: '#000',
     }
   }
