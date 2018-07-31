c8c2af95dc30ebc7f6215cd982680e24b94470fa
Jacob Janak <jakeathotmail@gmail.com>
2018-07-30 20:24:32 -0700

    Getting rid of steps folder.

--- client/src/components/BusinessBuilder/BusinessBuilder.js
+++ client/src/components/BusinessBuilder/BusinessBuilder.js
@@ -13,7 +13,7 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 // steps
-import Details from './steps/Details';
+import Details from './Details';
 
 const styles = theme => ({
   root: {
